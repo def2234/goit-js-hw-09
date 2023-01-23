bde0cd4a80ad99da9401ddd2633db1dda3117194
def2234 <110923837+def2234@users.noreply.github.com>
2023-01-23 15:50:27 +0000

    Deploying to gh-pages from @ def2234/goit-js-hw-09@8aab81e2f433a919e2abd63828dd75e8bb3f9746 🚀

---
+++ 01-color-switcher.595b914a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){0;a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}));var a=null;e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.595b914a.js.map
